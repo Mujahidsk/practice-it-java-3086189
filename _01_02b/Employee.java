b00f5e9ed6915922026ab5e68087d3151a640444
Mujahid Shaheed Shaik <mujahidshaik918@gmail.com>
2024-12-22 00:42:36 +0000

    solvedExcercise

--- _01_02b/Employee.java
+++ _01_02b/Employee.java
@@ -3,6 +3,26 @@ package _01_02b;
 public class Employee {
 
   public static void main(String[] args) {
+    int a = 29;
+    System.out.println("Age is " + a);
+
+    Boolean isAMANAGER = true;
+    System.out.println("isAMANAGER" + isAMANAGER);
+
+    double yearsOfService = 2.5;
+    System.out.println("yearsOfService" + yearsOfService);
+
+    int baseSalary = 3000;
+    System.out.println("baseSalary" + baseSalary);
+
+    int overtimePayment = 40;
+    System.out.println("overtimePayment" + overtimePayment);
+
+    int totalPayment = baseSalary + overtimePayment;
+    System.out.println("totalPayment" + totalPayment);
+
+    double firstBonus = 10.00, secondBonus = 22.00, thirdBonus = 35.00;
+    System.out.println("sum of firstBonus, secondBonus, thirdBonus" + (firstBonus + secondBonus + thirdBonus));
 
     // Create a variable called age of type int and assign it the value 29.
 
